8816429333bf2d7702cde5809f00f7af42f4a011
Karsten Larson <karsten.larson.1@gmail.com>
2025-02-26 11:49:39 -0600

    Added card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { Course } from './model/course';
+import { COURSES } from './data/course-data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CourseCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'practice-Component';
+  Csci160: Course = COURSES[0];
+  Csci161: Course = COURSES[1];
+  Csci213: Course = COURSES[2];
 }
